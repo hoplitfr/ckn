e009f69b4999ea3664715335d0a095cbb1cb6842
hoplitfr <contact@hoplit.fr>
2024-09-28 21:46:28 +0200

    Plugin creation
    This commit creates the Cool Kids Network and defines its constants.

--- wordpress/wp-content/plugins/cool-kids-network/index.php
+++ wordpress/wp-content/plugins/cool-kids-network/index.php
@@ -1,1 +1,16 @@
 <?php
+
+/**
+ * Plugin Name: Cool Kids Network
+ * Description: A Wordpress plugin to manage Cool Kids users
+ * Version: 1.0
+ * Author: Hoplitfr
+ */
+
+if (!defined('ABSPATH')) {
+    exit; // Exit if accessed directly
+}
+
+define('COOL_KIDS_PLUGIN_PATH', plugin_dir_path(__FILE__));
+define('COOL_KIDS_PLUGIN_URL', plugin_dir_url(__FILE__));
+
