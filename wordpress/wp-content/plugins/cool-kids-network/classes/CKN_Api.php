a660d7757e173480ad1f71d85c945ad037cbf537
hoplitfr <contact@hoplit.fr>
2024-10-02 23:34:43 +0200

    Register API route
    This commit defines a REST API route (/coolkids/v1/update-role/) to
    allow role updates via a POST request and adds rest_api_init action hook
    to initialize the route.

--- wordpress/wp-content/plugins/cool-kids-network/classes/CKN_Api.php
+++ wordpress/wp-content/plugins/cool-kids-network/classes/CKN_Api.php
@@ -5,5 +5,18 @@ class CKN_Api
     // Define your API key for validation
     private static $valid_api_key = '123456789';
 
+    /**
+     * Register the REST API route for updating user roles.
+     */
+    public static function register_role_update_route(): void
+    {
+        register_rest_route('coolkids/v1', '/update-role/', array(
+            'methods' => 'POST',
+            'callback' => [self::class, 'update_user_role_via_api'],
+            'permission_callback' => '__return_true', // No need for logged-in user, we handle security with the API key
+        ));
+    }
 }
 
+// Hook to initialize the custom REST route
+add_action('rest_api_init', [CKN_Api::class, 'register_role_update_route']);
